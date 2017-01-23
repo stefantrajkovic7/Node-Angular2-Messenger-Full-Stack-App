1a619a819dfe4d4b25627ac0711353b78ca1d9f2
saibot777 <stefantrajkovic777@gmail.com>
2017-01-23 17:16:22 +0100

    UI created for testing database connection

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
+});
+
+router.post('/', function(req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+      firstName: 'Stefan',
+      lastName: 'Trajkovic',
+      password: 'secret',
+      email: email
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
