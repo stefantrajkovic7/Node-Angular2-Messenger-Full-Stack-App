995ad7e405be6ae491e731a8798d6a30ca2f2475
saibot777 <stefantrajkovic777@gmail.com>
2017-01-24 17:02:04 +0100

    events added

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { Message } from './messages/message.model'
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    // message = {
-    //   content: 'A message',
-    //   author: 'Stefan'
-    // };
+    message: Message = new Message('Some message', 'Stefan');
 }
--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Message } from './message.model'
 
 @Component ({
   selector: 'app-message',
@@ -20,5 +21,10 @@ import { Component } from '@angular/core';
 })
 
 export class MessageComponent {
+  @Input() message: Message;
+  @Output() editClicked = new EventEmitter<string>();
 
+  onEdit() {
+    this.editClicked.emit('A new value');
+  }
 }
