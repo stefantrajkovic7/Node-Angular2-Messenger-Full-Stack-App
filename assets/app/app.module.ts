0efcd57e0aa989533225fd2c3bd44b8542358c9b
saibot777 <stefantrajkovic777@gmail.com>
2017-01-25 16:16:25 +0100

    data-driven form added

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header.component";
@@ -28,7 +28,7 @@ import { SigninComponent } from "./auth/signin.component";
         SignupComponent,
         SigninComponent
     ],
-    imports: [BrowserModule, FormsModule, routing],
+    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html'
 })
 
-export class SignupComponent {
-  
+export class SignupComponent implements OnInit {
+  myForm: FormGroup;
+
+  onSubmit() {
+    console.log(this.myForm);
+  }
+
+  ngOnInit () {
+    this.myForm = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+      ]),
+      password: new FormControl(null, Validators.required)
+    });
+  }
 }
