300a7740c56e5e33463746ede0a4109643c4651e
saibot777 <stefantrajkovic777@gmail.com>
2017-01-27 16:08:39 +0100

    signin auth

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -17,4 +17,12 @@ export class AuthService {
             .catch((error: Response) => Observable.throw(error.json()));
     }
 
+    signin(user: User) {
+        const body = JSON.stringify(user);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/user/signin', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
+
 }
--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+
+import { AuthService } from './auth.service';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-signin',
@@ -9,10 +13,21 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SigninComponent implements OnInit {
   myForm: FormGroup;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
   onSubmit() {
-    console.log(this.myForm);
-    this.myForm.reset();
-  }
+        const user = new User(this.myForm.value.email, this.myForm.value.password);
+        this.authService.signin(user)
+            .subscribe(
+                data => {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('userId', data.userId);
+                    this.router.navigateByUrl('/');
+                },
+                error => console.error(error)
+            );
+        this.myForm.reset();
+    }
 
   ngOnInit () {
     this.myForm = new FormGroup({
